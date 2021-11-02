7219827ff08afde9054bc239a1a40626fbe51059
carlospagno20 <carlos.pagno20@gmail.com>
2021-11-01 21:44:32 -0300

    postman

--- todolist/src/test/java/net/unibave/todolist/TodolistApplicationTests.java
+++ todolist/src/test/java/net/unibave/todolist/TodolistApplicationTests.java
@@ -1,6 +1,7 @@
 package net.unibave.todolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +10,9 @@ class TodolistApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+    public static void main(String[] args) {
+    	SpringApplication.run(TodolistApplication.class, args);
+    }
 
 }
