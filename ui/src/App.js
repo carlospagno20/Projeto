6e845f10601de2e9b69f877dc4e8c123682a6611
carlospagno20 <carlos.pagno20@gmail.com>
2021-11-08 21:24:17 -0300

    switch

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route} from 'react-router-dom';
+
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Button, Icon, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { useState } from 'react';
+
 
 function App() {
+  const [showMenu, setShowMenu] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BrowserRouter>
+
+      <div id="navbar" className={'nav-div'}>
+
+        <Button className={'nav-menu-item'} onClick={() => setShowMenu(!showMenu)}>
+          <Icon className={'nav-menu-item-icon'}>menu</Icon>
+        </Button>
+
+      </div>
+
+      <div className={'sidebar-main-div'}>
+
+        {showMenu && <div id="sidebar" className={'sidebar-left-div'}>
+
+          <List component="nav">
+
+            <ListItem button={true} component={Link} {...{to: "/pending"}}>
+              <ListItemIcon><Icon>list</Icon></ListItemIcon>
+              <ListItemText primary="Pending"></ListItemText>
+            </ListItem>
+            <ListItem button={true} component={Link} {...{to: "/done"}}>
+              <ListItemIcon><Icon>checked</Icon></ListItemIcon>
+              <ListItemText primary="Done"></ListItemText>
+            </ListItem>
+
+          </List>
+
+          </div>}
+
+        <div className={'sidebar-body-div'}>
+
+          <div className="sidebar-body-div-outlet">
+
+            <Switch>
+              <Route path="/pending" exact={true}><h1>Pending</h1></Route>
+              <Route path="/done" exact={true}><h1>Done</h1></Route> 
+              </Switch>
+
+</div>
+
+</div>
+
+</div>
+
+</BrowserRouter>
+</>
+);
 }
 
 export default App;
